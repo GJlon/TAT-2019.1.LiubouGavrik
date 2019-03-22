8fce3865a7931b0a8f9008c3fc787c66f8a3caed
GJlon <44772312+GJlon@users.noreply.github.com>
2019-03-22 12:03:16 +0200

    add comment

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace task_Dev_3
 {
     class EntryPoint
     {
+        /// <summary>
+        /// metod that allows to work with arguments from the command line
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns>everything is good - 0</returns>
+        /// <returns>return message if company doesn't have such specialists - 1</returns>
+        /// <returns>other exceptions in the program - 2</returns>
         static int Main(string[] args)
         {
             try
