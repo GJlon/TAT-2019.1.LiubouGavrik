9436763c535c6bcdef4bfe140cbc9b346d4e230f
GJlon <44772312+GJlon@users.noreply.github.com>
2019-03-27 17:30:33 +0200

    add comment

--- RealizationOfGuid.cs
+++ RealizationOfGuid.cs
@@ -2,8 +2,14 @@
 
 namespace task_Dev_4
 {
+    ///<summary>
+    ///static class for implementing a GUID parameter
+    ///<summary>
     static class RealizationOfGuid
     {
+       ///<summary>
+       ///special static method for GUID
+       ///<summary> 
         public static string GuidToString(this string str)
         {
             Guid guid = Guid.NewGuid();
