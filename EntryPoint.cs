7329245d6c94c7015b8f780da4b8a92083aba8bd
GJlon <44772312+GJlon@users.noreply.github.com>
2019-03-27 17:46:33 +0300

    add main class

---
+++ EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task_Dev_4
+{
+    class EntryPoint
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                string format ="PDF";
+                var lecture = new Lectures(format);
+                lecture[0] = "Cloud technologies";
+                lecture[1] = "Statistical radiophysics";
+                lecture[2] = "Information theory";
+                lecture[3] = "Cryptographic methods";
+
+                Console.WriteLine(lecture.Description());
+
+                return 0;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                return 1;
+            }
+            catch
+            {
+                Console.WriteLine("");
+                return 2;
+            }
+        }
+    }
+}
