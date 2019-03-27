a86af4649727668840b2d9fed0cf1ee19eae9e13
GJlon <44772312+GJlon@users.noreply.github.com>
2019-03-27 17:08:17 +0200

    add comments

--- LaboratoryWorks.cs
+++ LaboratoryWorks.cs
@@ -1,15 +1,13 @@
 ﻿namespace task_Dev_4
 {
+     /// <summary>
+     /// class heir to materials with overridden ToString () method
+     /// <summary>
     class LaboratoryWorks: Materials
     {
         public override string ToString()
         {
             return "Laboratory Work";
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
         }
     }
 }
