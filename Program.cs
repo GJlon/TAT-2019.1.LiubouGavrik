a5f13d28ac7ea66f87e5b8fa278a39c042c67008
GJlon <44772312+GJlon@users.noreply.github.com>
2019-04-05 02:58:32 +0300

    add main method

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Task_3
 {
+    /// <summary>
+    /// Entry point of this programm
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The main method of class Program.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Point point1 = new Point(3,4);
+            Point point2 = new Point(8,9);
+            Point point3 = new Point(11,15);
+
+            Builder mainBuilder = new RightTriangleBuilder(new EquilateralTriangeBuilder(new ArbitraryTriangleBuilder(null)));
+            Triangle triangle = mainBuilder.Build(point1, point2, point3);
+            Console.WriteLine(triangle.GetSquare());
         }
     }
 }
