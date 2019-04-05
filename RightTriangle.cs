76a006ee56d4aaaba051a0699c658f5d4bb99be5
GJlon <44772312+GJlon@users.noreply.github.com>
2019-04-05 03:55:29 +0300

    change logic in constructor of this class

--- RightTriangle.cs
+++ RightTriangle.cs
@@ -1,21 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Task_3
 {
+    /// <summary>
+    /// class right triangle, 
+    /// heir to class triangle overrides method for calculating area
+    /// </summary>
     class RightTriangle : Triangle
     {
-        public RightTriangle(Point point1, Point point2, Point point3) : base(point1, point2,point3 )
+        public RightTriangle(Point point1, Point point2, Point point3) : base(point1, point2, point3 )
         {
-            if( side12*side12+side23*side23-side31*side31 > float.Epsilon || side31 * side31 + side23 * side23 - side12 * side12 > float.Epsilon || side12 * side12 + side23 * side23 - side31 * side31 > float.Epsilon )
+            if (side12 * side12 + side23 * side23 - side31 * side31 < double.Epsilon && side31 * side31 + side23 * side23 - side12 * side12 < double.Epsilon)
             {
-                throw new Exception("This triangle isn't right triangle.");
+                throw new FormatException("This triangle isn't right triangle.");
+            }
+
+            else if (side12 * side12 + side23 * side23 - side31 * side31 < double.Epsilon && side12 * side12 + side23 * side23 - side31 * side31 < double.Epsilon)
+            {
+                throw new FormatException("This triangle isn't right triangle.");
+            }
+
+            else if (side31 * side31 + side12 * side12 - side23 * side23 < double.Epsilon && side12 * side12 + side23 * side23 - side31 * side31 < double.Epsilon)
+            {
+                throw new FormatException("This triangle isn't right triangle.");
             }
         }
 
+        /// <summary>
+        /// override implementation of method GetSquare()
+        /// </summary>
+        /// <returns></returns>
         public override double GetSquare()
         {
             double p = side12 + side23 + side31;
