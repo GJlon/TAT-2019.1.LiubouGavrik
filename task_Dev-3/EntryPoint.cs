ab08fab9417a71667f454d3ca700ce14690eaf9d
Jlon <juuliyakk@gmail.com>
2019-04-19 19:04:27 +0200

    add folder

---
+++ task_Dev-3/EntryPoint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task_Dev_3
+{
+    class EntryPoint
+    {
+        /// <summary>
+        /// metod that allows to work with arguments from the command line
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns>everything is good - 0</returns>
+        /// <returns>return message if company doesn't have such specialists - 1</returns>
+        /// <returns>other exceptions in the program - 2</returns>
+        static int Main(string[] args)
+        {
+            try
+            {
+                int budget = 20;
+                List<Employee> employeeList;
+                Company company = new Company();
+                employeeList = company.GetEmployees(new FirstCriteration(budget, company.employees));
+                foreach (var k in employeeList)
+                {
+                    Console.WriteLine(k);
+                }
+                return 0;
+            }
+            catch(OverflowException over)
+            {
+                Console.WriteLine(over.Message);
+                return 1;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error!!!");
+                return 2;
+            }
+        }
+    }
+}
---
+++ task_Dev-3/Senior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_Dev_3
+{
+    class Senior:Middle
+    {
+        private const int salary = 5;
+        private const int productivity = 7;
+        public override int GetSalary()
+        {
+            return salary;
+        }
+        public override int GetProductivity()
+        {
+            return productivity;
+        }
+    }
+}
