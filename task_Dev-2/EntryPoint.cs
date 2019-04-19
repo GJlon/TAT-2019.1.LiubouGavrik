e998a3535fc1f447b5559a8da335f6d2592931a8
Jlon <juuliyakk@gmail.com>
2019-04-19 18:14:06 +0200

    fix merge conflicts

---
+++ task_Dev-2/EntryPoint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace task_Dev_2
+{
+    /// <summary>
+    /// Class EntryPoint. Сlass for getting phonemes of the word
+    /// </summary>
+    class EntryPoint
+    {   /// <summary>
+        /// Main method of class EntryPoint
+        /// </summary>
+        /// <param name="args">Arguments from command line</param>
+        /// <returns>everything is good - 0</returns>
+        /// <returns>string has english letters of more then one "+" - 1</returns>
+        /// <returns>other exceptions in the program - 2</returns>
+        static int Main(string[] args)
+        {
+            try
+            {
+                string originalString = args[0].ToLower();
+                int length = originalString.Length;
+
+                Phoneme phoneme = new Phoneme(args[0]);
+                phoneme.CheckingEnglishLetters();
+                phoneme.CheckingPositionOfPlus();
+
+                Console.WriteLine(phoneme.ReturnString(length));
+                
+              return 0;
+            }
+            catch(FormatException exs)
+            {
+                Console.WriteLine(exs.Message);
+                
+              return 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+              return 2;
+            }
+        }
+    }
+}
