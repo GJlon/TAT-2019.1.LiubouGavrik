2092890b67d6ea8dd681523ac213b1b096917684
Jlon <juuliyakk@gmail.com>
2019-04-19 19:31:42 +0200

    add folder

---
+++ task_Dev-5/IFlyable.cs
@@ -0,0 +1,26 @@
+﻿namespace task_Dev_5
+{
+    /// <summary>
+    /// intrface IFlyable contains methods that will be used in classes Bird, SpaceShip, Plane
+    /// </summary>
+    interface IFlyable
+    {
+        /// <summary>
+        /// Method for working with points of reference and arrival (distance)
+        /// </summary>
+        /// <param name="newPoint"></param>
+        void FlyTo(Point newPoint);
+
+        /// <summary>
+        /// Object initialization method
+        /// </summary>
+        /// <returns></returns>
+        string WhoAmI();
+
+        /// <summary>
+        /// Method to get flight time
+        /// </summary>
+        /// <returns></returns>
+        double GetFlyTime();
+    }
+}
