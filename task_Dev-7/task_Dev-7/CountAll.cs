d6ca6e20e001dfe4c3104b79f24c851363148e3c
GJlon <44772312+GJlon@users.noreply.github.com>
2019-05-08 01:26:12 +0300

    add accessibility modifier

--- task_Dev-7/task_Dev-7/CountAll.cs
+++ task_Dev-7/task_Dev-7/CountAll.cs
@@ -5,7 +5,7 @@ namespace task_Dev_7
     /// <summary>
     /// Class which describes total number of cars
     /// </summary>
-    class CountAll : Command
+    internal class CountAll : Command
     {
         InfoDocument readDocument;
 
